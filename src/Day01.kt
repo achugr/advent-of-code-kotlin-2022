bb0d857859e12b95690020fbdd65487bc937d9ad
artemii.chugreev <artemij.chugreev@gmail.com>
2022-12-01 22:19:20 -0500

    Day 1 task

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
+    fun groupAndSum(input: List<String>) =
+        input.foldIndexed(mutableListOf<MutableList<Int>>(mutableListOf())) { index, list, value ->
+            when {
+                value.isBlank() -> list.add(mutableListOf())
+                list.isNotEmpty() -> list.last().add(value.toInt())
+                else -> list.add(mutableListOf())
+            }
+            list
+        }
+            .filter { list -> list.isNotEmpty() }
+            .map { list -> list.sum() }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return groupAndSum(input)
+            .max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return groupAndSum(input)
+            .sorted()
+            .reversed()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val testInput = readInput("Day01")
+    println(part1(testInput))
+    println(part2(testInput))
 }
